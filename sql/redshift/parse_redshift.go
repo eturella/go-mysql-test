9500b97c4b7b79a296b177a8707c0245c4ef9a9f
turella <turella@MBP-di-sysinfo.homenet.telecomitalia.it>
2019-12-06 00:57:51 +0100

    valorizzazione della struttura dati da passare alla API di esecuzione del COPY

--- sql/redshift/parse_redshift.go
+++ sql/redshift/parse_redshift.go
@@ -5,11 +5,13 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/bisegni/go-c-interface-test/rscopy"
+
 	"github.com/araddon/qlbridge/lex"
 )
 
 // ParseCopy parses the COPY staatement, using the redshift dialect
-func ParseCopy(sql string) bool {
+func ParseCopy(sql string) (*rscopy.RsCopy, bool) {
 	sql = strings.TrimSpace(sql)
 	sql = strings.Replace(sql, "format as ", "format ", 1)
 	sql = strings.Replace(sql, "delimiter as ", "delimiter ", 1)
@@ -18,9 +20,10 @@ func ParseCopy(sql string) bool {
 	sql = strings.Replace(sql, "\r", " ", 1)
 	sql = strings.Replace(sql, "\n", " ", 1)
 	if len(sql) <= 0 {
-		return false
+		return nil, false
 	}
 	l := NewRedshiftLexer(sql)
+	res := rscopy.RsCopy{}
 	tok := l.NextToken()
 	for tok.T != lex.TokenEOF {
 		fmt.Printf("got:%v  \n", tok)
@@ -32,9 +35,10 @@ func ParseCopy(sql string) bool {
 			fmt.Printf(" -> Statement = %s \n", tok.V)
 			tok = l.NextToken()
 			if tok.T != lex.TokenTable {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Tabella dati = %s \n", tok.V)
+			res.Table = tok.V
 			// Riconoscimento della lista dei campi
 			l.SkipWhiteSpacesNewLine()
 			w := l.PeekWord()
@@ -58,23 +62,27 @@ func ParseCopy(sql string) bool {
 					i++
 				}
 				fmt.Printf(" -> Elenco campi = %v \n", fl)
+				res.Fields = fl
 			}
 		} else if tok.T == lex.TokenFrom {
 			// Riconoscimento del tipo FROM-TO e parametro
 			fmt.Printf(" -> Direzione copia = %s \n", tok.V)
 			tok = l.NextToken()
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Sorg/Dest esterna = %s \n", tok.V)
+			res.Source.SourceURI = tok.V
 		} else if tok.T == TokenCIamRole || (tok.T == lex.TokenIdentity && tok.V == "iam_role") {
 			// Riconoscimento del parametro di autenticazione
 			fmt.Printf(" -> Autenticazione tipo = %s \n", tok.V)
 			tok = l.NextToken()
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Autenticazione valore = %s \n", tok.V)
+			//a := rscopy.Auth{IamRole: tok.V}
+			res.Source.ExternalAuth.IamRole = tok.V
 		} else if tok.T == TokenCRegion || (tok.T == lex.TokenIdentity && tok.V == "region") {
 			// Riconoscimento del parametro "region"
 			fmt.Printf(" -> Origine = %s \n", tok.V)
@@ -85,6 +93,7 @@ func ParseCopy(sql string) bool {
 			}
 			tok = l.NextToken()
 			fmt.Printf(" -> Origine parametro = %s \n", tok.V)
+			res.Source.Region = tok.V
 		} else if tok.T == TokenCFormatAs {
 			// Riconoscimento del parametro "format"
 			fmt.Printf(" -> Formato = %s \n", tok.V)
@@ -94,8 +103,16 @@ func ParseCopy(sql string) bool {
 				tok = l.NextToken()
 			}
 			tok = l.NextToken()
-			if tok.T != TokenCFormatParquet && tok.T != TokenCFormatAvro {
-				return false
+			//if tok.T != TokenCFormatParquet && tok.T != TokenCFormatAvro {
+			//	return false
+			//}
+			switch tok.T {
+			case TokenCFormatParquet:
+				res.Format.Type = rscopy.PARQUET
+			case TokenCFormatAvro:
+				res.Format.Type = rscopy.AVRO
+			default:
+				return nil, false
 			}
 			fmt.Printf(" -> Formato tipologia = %s \n", tok.V)
 			opt := "auto"
@@ -112,9 +129,11 @@ func ParseCopy(sql string) bool {
 				opt = tok.V
 			}
 			fmt.Printf(" -> Formato tipologia_option = %s \n", opt)
+			res.Format.Option = opt
 		} else if tok.T == lex.TokenIdentity && tok.V == "json" {
 			// Riconoscimento del formato "json"
 			fmt.Printf(" -> Formato = %s \n", tok.V)
+			res.Format.Type = rscopy.JSON
 			opt := "auto"
 			l.SkipWhiteSpacesNewLine()
 			w := l.PeekWord()
@@ -129,41 +148,51 @@ func ParseCopy(sql string) bool {
 				opt = tok.V
 			}
 			fmt.Printf(" -> Formato json_option = %s \n", opt)
+			res.Format.Option = opt
 		} else if tok.T == TokenCCsv || tok.T == lex.TokenIdentity && tok.V == "csv" {
 			// Riconoscimento del formato "csv"
 			fmt.Printf(" -> Formato = %s \n", tok.V)
+			res.Format.Type = rscopy.CSV
 		} else if tok.T == TokenCFixedwidth {
 			// Riconoscimento del formato "fixedwidth"
 			fmt.Printf(" -> Formato = %s \n", tok.V)
+			res.Format.Type = rscopy.FIXEDWIDTH
 			tok = l.NextToken()
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Formato fixedwidth_spec = %s \n", tok.V)
-			tmp := strings.Split(tok.V, ",")
-			m := make(map[string]int, len(tmp))
-			for _, t := range tmp {
-				if strings.Contains(t, ":") {
-					tmp := strings.Split(t, ":")
-					v, err := strconv.Atoi(tmp[1])
-					if err != nil {
-						return false
-					}
-					m[tmp[0]] = v
-				}
-			}
-			fmt.Printf(" -> Formato fixedwidth_spec = %+v \n", m)
+			res.Format.Option = tok.V
+			// tmp := strings.Split(tok.V, ",")
+			// m := make(map[string]int, len(tmp))
+			// for _, t := range tmp {
+			// 	if strings.Contains(t, ":") {
+			// 		tmp := strings.Split(t, ":")
+			// 		v, err := strconv.Atoi(tmp[1])
+			// 		if err != nil {
+			// 			return false
+			// 		}
+			// 		m[tmp[0]] = v
+			// 	}
+			// }
+			// fmt.Printf(" -> Formato fixedwidth_spec = %+v \n", m)
 		} else if tok.T == TokenCSSH || (tok.T == lex.TokenIdentity && tok.V == "ssh") {
 			// Riconoscimento del data-source "ssh"
 			fmt.Printf(" -> Origine file = %s \n", tok.V)
+			res.Source.SSH = true
 		} else if tok.T == TokenCReadratio {
-			// Riconoscimento del parametro "readration" (dynamoDB)
+			// Riconoscimento del parametro "readratio" (dynamoDB)
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
 			tok = l.NextToken()
 			if tok.T != lex.TokenInteger {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Opzione parametro = %s \n", tok.V)
+			r, err := strconv.Atoi(tok.V)
+			if err != nil {
+				return nil, false
+			}
+			res.Format.Readratio = r
 		} else if tok.T == TokenCQuoteAs {
 			// Riconoscimento del parametro "quote"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
@@ -172,9 +201,10 @@ func ParseCopy(sql string) bool {
 				tok = l.NextToken()
 			}
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Opzione parametro = %s \n", tok.V)
+			res.Format.Quote = tok.V
 		} else if tok.T == TokenCDelimiter {
 			// Riconoscimento del parametro "delimiter"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
@@ -185,9 +215,10 @@ func ParseCopy(sql string) bool {
 			}
 			// fmt.Printf(" 2. %s\n", tok.V)
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Opzione parametro = %s \n", tok.V)
+			res.Format.Delimiter = tok.V
 		} else if tok.T == TokenCTimeformat {
 			// Riconoscimento del parametro "timeformat"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
@@ -196,32 +227,43 @@ func ParseCopy(sql string) bool {
 				tok = l.NextToken()
 			}
 			if tok.T != lex.TokenValue {
-				return false
+				return nil, false
 			}
 			fmt.Printf(" -> Opzione parametro = %s \n", tok.V)
+			res.Format.Timeformat = tok.V
+		} else if tok.T == TokenCLzop {
+			// Riconoscimento dei formati di compressione
+			fmt.Printf(" -> Compressione file = %s \n", tok.V)
+			res.Format.Compression = rscopy.LZOP
 		} else if tok.T == TokenCLzop || tok.T == TokenCGzip {
 			// Riconoscimento dei formati di compressione
 			fmt.Printf(" -> Compressione file = %s \n", tok.V)
+			res.Format.Compression = rscopy.GZIP
 		} else if tok.T == TokenCManifest || (tok.T == lex.TokenIdentity && tok.V == "manifest") {
 			// Riconoscimento del parametro "manifest"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
+			res.Source.Manifest = true
 		} else if tok.T == TokenCEncripted || (tok.T == lex.TokenIdentity && tok.V == "encrypted") {
 			// Riconoscimento del parametro "encrypted"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
+			res.Source.Encrypted = true
 		} else if tok.T == TokenCExplicitIds || (tok.T == lex.TokenIdentity && tok.V == "explicit_ids") {
 			// Riconoscimento del parametro "explicit_ids"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
+			res.Format.ExplicitIds = true
 		} else if tok.T == TokenCEscape || (tok.T == lex.TokenIdentity && tok.V == "escape") {
 			// Riconoscimento del parametro "escape"
 			fmt.Printf(" -> Opzione = %s \n", tok.V)
+			res.Format.Escape = true
 		} else if tok.T == lex.TokenEOS {
 			// Riconoscimento del punto e virgola finale
 			break
 		} else {
 			// Casistiche non gestite
 			fmt.Printf(" ** non riconosciuto = %s \n", tok.V)
+			return nil, false
 		}
 		tok = l.NextToken()
 	}
-	return true
+	return &res, true
 }
--- sql/redshift/parse_redshift_test.go
+++ sql/redshift/parse_redshift_test.go
@@ -1,6 +1,7 @@
 package redshift
 
 import (
+	"encoding/json"
 	"errors"
 	"strings"
 	"testing"
@@ -10,10 +11,12 @@ func verifyParser(q string, t *testing.T) error {
 	q = strings.TrimSpace(q)
 	q = strings.Replace(q, "json", "myjson", -1)
 
-	ok := ParseCopy(q)
+	p, ok := ParseCopy(q)
 	if !ok {
-		return errors.New("...")
+		return errors.New("Parsing fallito")
 	}
+	ps, _ := json.MarshalIndent(p, "", "	")
+	t.Logf("Result:\n%s\n", ps)
 	return nil
 }
 
@@ -21,7 +24,7 @@ func TestCopyFromParser_1(t *testing.T) {
 	t.Logf("query: %v\n\n", q1)
 	err := verifyParser(q1, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -30,7 +33,7 @@ func TestCopyFromParser_2(t *testing.T) {
 	t.Logf("query: %v\n\n", q2)
 	err := verifyParser(q2, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -39,7 +42,7 @@ func TestCopyFromParser_3(t *testing.T) {
 	t.Logf("query: %v\n\n", q3)
 	err := verifyParser(q3, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -48,7 +51,7 @@ func TestCopyFromParser_4(t *testing.T) {
 	t.Logf("query: %v\n\n", q4)
 	err := verifyParser(q4, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -57,7 +60,7 @@ func TestCopyFromParser_5(t *testing.T) {
 	t.Logf("query: %v\n\n", q5)
 	err := verifyParser(q5, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -66,7 +69,7 @@ func TestCopyFromParser_6(t *testing.T) {
 	t.Logf("query: %v\n\n", q6)
 	err := verifyParser(q6, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -75,7 +78,7 @@ func TestCopyFromParser_7(t *testing.T) {
 	t.Logf("query: %v\n\n", q7)
 	err := verifyParser(q7, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -84,7 +87,7 @@ func TestCopyFromParser_8(t *testing.T) {
 	t.Logf("query: %v\n\n", q8)
 	err := verifyParser(q8, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -93,7 +96,7 @@ func TestCopyFromParser_9(t *testing.T) {
 	t.Logf("query: %v\n\n", q9)
 	err := verifyParser(q9, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -102,7 +105,7 @@ func TestCopyFromParser_10(t *testing.T) {
 	t.Logf("query: %v\n\n", q10)
 	err := verifyParser(q10, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -111,7 +114,7 @@ func TestCopyFromParser_11(t *testing.T) {
 	t.Logf("query: %v\n\n", q11)
 	err := verifyParser(q11, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -120,7 +123,7 @@ func TestCopyFromParser_12(t *testing.T) {
 	t.Logf("query: %v\n\n", q12)
 	err := verifyParser(q12, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -129,7 +132,7 @@ func TestCopyFromParser_13(t *testing.T) {
 	t.Logf("query: %v\n\n", q13)
 	err := verifyParser(q13, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -138,7 +141,7 @@ func TestCopyFromParser_14(t *testing.T) {
 	t.Logf("query: %v\n\n", q14)
 	err := verifyParser(q14, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -147,7 +150,7 @@ func TestCopyFromParser_15(t *testing.T) {
 	t.Logf("query: %v\n\n", q15)
 	err := verifyParser(q15, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -156,7 +159,7 @@ func TestCopyFromParser_16(t *testing.T) {
 	t.Logf("query: %v\n\n", q16)
 	err := verifyParser(q16, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -165,7 +168,7 @@ func TestCopyFromParser_17(t *testing.T) {
 	t.Logf("query: %v\n\n", q17)
 	err := verifyParser(q17, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -174,7 +177,7 @@ func TestCopyFromParser_18(t *testing.T) {
 	t.Logf("query: %v\n\n", q18)
 	err := verifyParser(q18, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -183,7 +186,7 @@ func TestCopyFromParser_19(t *testing.T) {
 	t.Logf("query: %v\n\n", q19)
 	err := verifyParser(q19, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -192,7 +195,7 @@ func TestCopyFromParser_20(t *testing.T) {
 	t.Logf("query: %v\n\n", q20)
 	err := verifyParser(q20, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -201,7 +204,7 @@ func TestCopyFromParser_21(t *testing.T) {
 	t.Logf("query: %v\n\n", q21)
 	err := verifyParser(q21, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -210,7 +213,7 @@ func TestCopyFromParser_22(t *testing.T) {
 	t.Logf("query: %v\n\n", q22)
 	err := verifyParser(q22, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -219,7 +222,7 @@ func TestCopyFromParser_23(t *testing.T) {
 	t.Logf("query: %v\n\n", q23)
 	err := verifyParser(q23, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -228,7 +231,7 @@ func TestCopyFromParser_24(t *testing.T) {
 	t.Logf("query: %v\n\n", q24)
 	err := verifyParser(q24, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -237,7 +240,7 @@ func TestCopyFromParser_25(t *testing.T) {
 	t.Logf("query: %v\n\n", q25)
 	err := verifyParser(q25, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -246,7 +249,7 @@ func TestCopyFromParser_26(t *testing.T) {
 	t.Logf("query: %v\n\n", q26)
 	err := verifyParser(q26, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -255,7 +258,7 @@ func TestCopyFromParser_27(t *testing.T) {
 	t.Logf("query: %v\n\n", q27)
 	err := verifyParser(q27, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
@@ -264,7 +267,7 @@ func TestCopyFromParser_28(t *testing.T) {
 	t.Logf("query: %v\n\n", q28)
 	err := verifyParser(q28, t)
 	if err != nil {
-		t.Logf("got:%v  \n", err)
+		t.Logf("%v  \n", err)
 		t.FailNow()
 	}
 }
