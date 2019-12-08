be20ea579fcc94c5e180d79381a228e4ec249c62
turella <turella@MBP-di-sysinfo.homenet.telecomitalia.it>
2019-12-08 02:07:56 +0100

    WIP gestione delle query utilizzate dai client grafici per consultare il db

---
+++ bisegniadapter/showdatabases.go
@@ -0,0 +1,64 @@
+package bisegniadapter
+
+import (
+	"sort"
+	"strings"
+
+	"github.com/src-d/go-mysql-server/sql"
+)
+
+// ShowDatabases is a node that shows the databases.
+type ShowDatabases struct {
+}
+
+// NewShowDatabases creates a new show databases node.
+func NewShowDatabases() *ShowDatabases {
+	return new(ShowDatabases)
+}
+
+// Resolved implements the Resolvable interface.
+func (p *ShowDatabases) Resolved() bool {
+	return true
+}
+
+// Children implements the Node interface.
+func (*ShowDatabases) Children() []sql.Node {
+	return nil
+}
+
+// Schema implements the Node interface.
+func (*ShowDatabases) Schema() sql.Schema {
+	return sql.Schema{{
+		Name:     "Database",
+		Type:     sql.Text,
+		Nullable: false,
+	}}
+}
+
+// RowIter implements the Node interface.
+func (p *ShowDatabases) RowIter(ctx *sql.Context) (sql.RowIter, error) {
+	dbs := []string{"default"}
+	var rows = make([]sql.Row, 0, len(dbs))
+	for _, db := range dbs {
+		rows = append(rows, sql.Row{db})
+	}
+
+	sort.Slice(rows, func(i, j int) bool {
+		return strings.Compare(rows[i][0].(string), rows[j][0].(string)) < 0
+	})
+
+	return sql.RowsToRowIter(rows...), nil
+}
+
+// WithChildren implements the Node interface.
+func (p *ShowDatabases) WithChildren(children ...sql.Node) (sql.Node, error) {
+	if len(children) != 0 {
+		return nil, sql.ErrInvalidChildrenNumber.New(p, len(children), 0)
+	}
+
+	return p, nil
+}
+
+func (p ShowDatabases) String() string {
+	return "ShowDatabases"
+}
--- sql/parse/parse.go
+++ sql/parse/parse.go
@@ -1,7 +1,10 @@
 package parse
 
 import (
+	"fmt"
 	"strings"
+
+	"github.com/araddon/qlbridge/rel"
 
 	"github.com/eturella/go-mysql-test/sql"
 	"github.com/eturella/go-mysql-test/sql/plan"
@@ -22,6 +25,32 @@ func Parse(ctx *sql.Context, query string) (sql.Node, error) {
 		ctx.Warn(0, "query was empty after trimming comments, so it will be ignored")
 		// return plan.Nothing, nil
 		panic("")
+	}
+
+	// l := lex.NewSqlLexer(query)
+	// tok := l.NextToken()
+	// for tok.T != lex.TokenEOF {
+	// 	fmt.Printf("got:%v  \n", tok)
+	// 	tok = l.NextToken()
+	// }
+
+	sqlRequest, err := rel.ParseSql(query)
+	if err != nil {
+		fmt.Printf("%v \n", err)
+	}
+	fmt.Printf("%v \n", sqlRequest)
+	// fmt.Printf("%s \n", json.MarshalIndent(sqlRequest, "", "    "))
+	switch sqlRequest.(type) {
+	case *rel.SqlShow:
+		stmt := sqlRequest.(*rel.SqlShow)
+		fmt.Printf("%+v \n", *stmt)
+		showType := strings.ToLower(stmt.ShowType)
+		fmt.Printf("%+v \n", showType)
+		// {Raw:SHOW full tables from qualcosa like 'utente@%' Db:qualcosa Full:true Scope:
+		// ShowType:tables From: Identity: Create:false CreateWhat: Where:<nil> Like:Table LIKE "utente@%"}
+
+		// {Raw:SHOW databases like 'utente@%' Db: Full:false Scope: S
+		// howType:databases From: Identity: Create:false CreateWhat: Where:<nil> Like:<nil>}
 	}
 
 	return plan.NewUnresolvedTable("mytable", "mydb"), nil
